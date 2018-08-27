7f68c9a91bb73179c84ea6d3955d4c5e1810ef32
EduardoZimermam <eduardozimerman@live.com>
2018-08-27 14:59:15 -0300

    Implementação em andamento

--- Classifiers.py
+++ Classifiers.py
@@ -2,32 +2,44 @@
 # -*- coding: utf-8 -*-
 
 from sklearn import datasets
-import numpy as np
-from sklearn.neighbors import NearestNeighbors
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import KFold
+from sklearn.metrics import accuracy_score
+import matplotlib.pyplot as plt
+from sklearn.model_selection import cross_val_score
 
 # Load do dataset
 iris = datasets.load_iris()
 
-# Extração de 4 características
-X = iris.data[:, :4]
+# Extração de 4 características com as 3 classes do Iris Dataset.
+X = iris.data
 y = iris.target
 
-# Para a divisão do database em porcentagens de treinamento e de teste. EXECUTAR 30x.
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=0)
+range_k = range(1, 30)
+scores=[]
 
-neigh = NearestNeighbors(2, 0.4)
-# neigh.fit(X)
-# train = neigh.kneighbors(X, return_distance=False)
+for k in range_k:
+	neigh = KNeighborsClassifier(k)
+	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=0)
+	neigh.fit(X_train, y_train)
+	scores.append(accuracy_score(y_test, neigh.predict(X_test)))
 
-kf = KFold(n_splits=5)
-for train, test in kf.split(X):
-	X_train, X_test = X[train], X[test]
-   	y_train, y_test = y[train], y[test]
-    
-neigh.fit(X_train)
-print(neigh.kneighbors(X_test, return_distance=False))
+plt.plot(range_k, scores)
+plt.xlabel('K')
+plt.ylabel('Acuracia')
+plt.title('Usando 60% para treinamento e 40% para')
+plt.show()
 
-# print(cv_results)
+scores = []
 
+for k in range_k:
+	neigh = KNeighborsClassifier(k)
+	neigh.fit(X, y)
+	scores.append(cross_val_score(neigh, X, y, cv=5).mean())
+
+plt.plot(range_k, scores)
+plt.xlabel('K')
+plt.ylabel('Acuracia')
+plt.title('Usando 5 folds')
+plt.show()
